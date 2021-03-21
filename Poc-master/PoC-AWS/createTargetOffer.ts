fff36748e85e2f1e252814fe77ad0945e79f59c2
Akash Mane <akashmane1434@gmail.com>
2021-03-21 22:26:38 +0530

    AWS Task

---
+++ Poc-master/PoC-AWS/createTargetOffer.ts
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk")
+export const handler = async (event: any, context: any, callback: any) => {
+    let awsConfig = {
+        "region": process.env.REGION,
+        "endpoint": process.env.END_POINT,
+        "accessKeyId": process.env.ACCESS_KEY_ID, "secretAccessKey": process.env.SECRET_KEY
+    }
+    AWS.config.update({ awsConfig });
+    //create the dunamodb service object
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    const tarId = Math.round(Math.random()*1000000);
+    var currentDate = new Date();
+    
+   const params:any = {
+       TableName:"user",
+       Item: { 
+           targetId: tarId,
+           userId:145098,
+           promotionId:918850,
+           creationTime:currentDate.getTime()
+        }
+   }
+   try{
+    const data = await documentClient.put(params).promise();
+   }catch(err){
+       console.log(err);   
+   }
+};
+
+
---
+++ Poc-master/PoC-AWS/putData.ts
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk")
+export const handler = async (event: any, context: any, callback: any) => {
+
+    let awsConfig = {
+        "region": process.env.REGION,
+        "endpoint": process.env.END_POINT,
+        "accessKeyId": process.env.ACCESS_KEY_ID, "secretAccessKey": process.env.SECRET_KEY
+    }
+    AWS.config.update({ awsConfig });
+    //create the dunamodb service object
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    const promoId = Math.round(Math.random()*1000000);
+    const jobStartTime = Math.floor(new Date().getTime()/1000.0);
+
+   const params:any = {
+       TableName:"promotionoffer",
+       Item: { 
+           promotionId: promoId,
+           name:"Suraj",
+           minAge:20,
+           maxAge:50,
+           gender:"MALE",
+           jobStartTimeEpoch:jobStartTime,
+           jobEndTimeEpoch:254512586
+        }
+   }
+   try{
+    const data = await documentClient.put(params).promise();
+   }catch(err){
+       console.log(err);   
+   }
+};
+
+
